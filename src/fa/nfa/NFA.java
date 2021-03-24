6a3e59b793dec1bcc0927a70aa7b5d46d22401c5
Justin Raver <justinraver113@gmail.com>
2021-03-24 17:53:49 -0600

    Initial build and comments for all methods

--- src/fa/nfa/NFA.java
+++ src/fa/nfa/NFA.java
@@ -3,61 +3,141 @@ package fa.nfa;
 import fa.State;
 import fa.dfa.DFA;
 
-import java.util.Set;
+import java.util.*;
 
+/**
+ * @author JustinRaver, NickStolarow
+ * @version 1.0
+ */
 public class NFA implements NFAInterface {
+
+    //instance variables
+    private final Set<State> states;
+    private final Set<Character> alphabet;
+    private State startState;
+    private final Set<State> finalStates;
+
+    /**
+     * Basic Constructor
+     */
+    public NFA(){
+        this.states = new LinkedHashSet<>();
+        this.alphabet = new HashSet<>();
+        this.finalStates = new HashSet<>();
+        this.startState = null;
+    }
     @Override
     public void addStartState(String name) {
+        this.startState = new NFAState(name);
 
+        if(getState(name) == null){
+            states.add(new NFAState(name));
+        }
     }
 
     @Override
     public void addState(String name) {
-
+        states.add(new NFAState(name));
     }
 
     @Override
     public void addFinalState(String name) {
+        finalStates.add(new NFAState(name));
 
+        if(getState(name) == null){
+            states.add(new NFAState(name));
+        }
     }
 
     @Override
-    public void addTransition(String fromState, char onSymb, String toState) {
-
+    public void addTransition(String fromState, char onSym, String toState) {
+            Objects.requireNonNull(getState(fromState)).setTransitions(String.valueOf(onSym), toState);
     }
 
     @Override
     public Set<? extends State> getStates() {
-        return null;
+        return states;
     }
 
     @Override
     public Set<? extends State> getFinalStates() {
-        return null;
+        return finalStates;
     }
 
     @Override
     public State getStartState() {
-        return null;
+        return startState;
     }
 
     @Override
     public Set<Character> getABC() {
-        return null;
+        return alphabet;
     }
 
     @Override
     public DFA getDFA() {
+        //DFA to build
+        DFA dfa = new DFA();
+        //create queue for breadth first search
+        Queue<NFAState> q = new LinkedList<>();
+        //queue start state
+        q.add(startState);
+        dfa.addFinalState();
+        //dequeue and queue all child nodes
         return null;
     }
 
     @Override
     public Set<NFAState> getToState(NFAState from, char onSymb) {
-        return null;
+        return from.getTransitions(String.valueOf(onSymb));
     }
 
     @Override
     public Set<NFAState> eClosure(NFAState s) {
+        //Depth first algorithm:
+        Set<NFAState> set = new HashSet<>();
+        set.add(s);
+        // create stack push root node to stack
+        Stack<NFAState> stack = new Stack<>();
+        stack.push(s);
+
+        while(!stack.empty()){
+            // pop node and push all child nodes onto stack
+            // repeat popping the top node and pushing its children
+            //when stack is empty return list of nodes reached on e
+            for(NFAState state: stack.pop().getTransitions("e")) {
+                stack.push(state);
+                set.add(state);
+            }
+        }
+        return set;
+    }
+
+    /**
+     *
+     * @param s name of the state
+     * @return a state with the name passed or null if it doesnt exist
+     */
+    private NFAState getState(String s) {
+        for (State st : states) {
+            if (st.toString().equals(s)) {
+                return (NFAState) st;
+            }
+        }
         return null;
+    }
+
+    /**
+     *
+     * @param state the state to be compared
+     * @return true if state is final false otherwise
+     */
+    private boolean isFinal(NFAState state){
+        for(NFAState s:finalStates){
+            if(s.equals(state)){
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/fa/nfa/NFAState.java
+++ src/fa/nfa/NFAState.java
@@ -1,7 +1,66 @@
 package fa.nfa;
 
 import fa.State;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+/**
+ * @author JustinRaver, NickStolarow
+ * @version 1.0
+ */
 public class NFAState extends State {
 
+    //instance variables
+    private Map<String, Set<NFAState>> transitions;
+
+    /**
+     * Basic Constructor
+     */
+    public NFAState(String name){
+        transitions = new HashMap<>();
+        super.name = name;
+    }
+
+    /**
+     *
+     * @return the States transitions
+     */
+    public Set<NFAState> getTransitions(String s){
+        return this.transitions.get(s);
+    }
+
+    /**
+     *
+     * @param sym the symbol being transitioned on
+     * @param state the state we transition to on sym
+     */
+    public void setTransitions(String sym,String state) {
+        if(transitions.containsKey(sym)){
+            updateMapSet(sym,state);
+        }else{
+            Set<NFAState> set = new HashSet<>();
+            set.add(new NFAState(state));
+            transitions.put(sym,set);
+        }
+    }
+
+    /**
+     *
+     * @param sym the symbol being transitioned on
+     * @param state the state we transition to on sym
+     */
+    public void updateMapSet(String sym,String state){
+        transitions.get(sym).add(new NFAState(state));
+    }
+
+    /**
+     * Comparison method for NFAStates
+     * @param nfaState another nfastate
+     * @return true if state names match false otherwise
+     */
+    public boolean equals(NFAState nfaState){
+        return nfaState.toString().equals(this.toString());
+    }
 }
